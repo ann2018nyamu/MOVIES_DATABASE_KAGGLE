d01bdb18a895b75fd12e4ecb9eacad27cdbb9b6e
Ann Nyamu <anyamu@hq.ekenya.co.ke>
2025-01-22 17:17:01 +0300

    Records inserted in Tables

--- Movies_Test.sql
+++ Movies_Test.sql
@@ -30,18 +30,71 @@ CategoryName Varchar(255),
 Description Varchar(255)
 );
 
+
+CREATE TABLE Shippers_Table(
+ShipperID INT primary key,
+ShipperName VARCHAR(255),
+Phone CHAR (10)
+);
+
+CREATE TABLE Clients_Table(
+ClientID INT Primary key,
+ClientName VARCHAR(255),
+ContactName VARCHAR(255),
+Address VARCHAR(255),
+City VARCHAR(255),
+PostalCode INT,
+Country VARCHAR(255)
+);
+
+CREATE TABLE Suppliers_Table(
+SupplierID INT primary key,
+SupplierName VARCHAR(255),
+ContactName VARCHAR(255),
+Address VARCHAR(255),
+City VARCHAR(255),
+PostalCode VARCHAR(255),
+Country VARCHAR(255)
+);
+
+
+INSERT INTO Clients_Table(ClientID, ClientName, ContactName, Address, City, PostalCode, Country)
+VALUES
+(1, 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', 12209, 'Germany' ),
+(2, 'Ana Trujillo Helados', 'Ana Trujillo', 'Avda. de la Constitution 2222', 'Mexico D.F', 05021, 'Mexico'),
+(3, 'Antonio Moreno Taqueria', 'Antonio Moreno', 'Mataderos 2312', 'Mexico D.F', 06023, 'Mexico');
+
+
+INSERT INTO Suppliers_Table(SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country)
+VALUES
+(1, 'Exotic Liquid', 'Charlotte Cooper', '49 Gilbert St.', 'London', 'EC1 4SD', 'UK'),
+(2, 'New Orleans Cajun Delights', 'Shelley Burke', 'P.O Box 78934', 'New Orleans', '70117', 'USA'),
+(3, 'Grandma Kellys Homestead', 'Regina Murphy', '707 Oxford Rd.', 'Ann Arbor', '48104', 'USA');
+ 
+INSERT INTO Clients_Table(ClientID, ClientName, ContactName, Address, City, PostalCode, Country)
+VALUES 
+(4, 'Anatonio Moreno Taqueria', 'Antonio Moreno', 'Mataderos 2412', 'Mexico R.F', 07023, 'Kenya'),
+(5, 'Annitonio Moreno Taqueria', 'Antonio Moreno', 'Mataderos 2512', 'Mexico L.F', 08023, 'USA'),
+(6, 'Moreno Taqueria', 'Antonio Moreno', 'Mataderos 2612', 'Mexico T.F', 09023, 'Mexico'),
+(7, 'Trontonio Moreno Taqueria', 'Antonio Moreno', 'Mataderos 2712', 'Mexico M.F', 10023, 'Mexico');
+
 INSERT INTO product_table(ProductID, ProductName, CategoryID, Price)
 VALUES
 (1, 'Chais', 1, 18),
 (2, 'Chang', 1, 19),
 (3, 'AniseedSyrup', 2, 20);
 
-
 INSERT INTO categories_table(CategoryID, CategoryName, Description)
 VALUES
 (1, 'Beverages', 'Soft drinks, coffees, teas, beers, and ales'),
 (2, 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings'),
 (3, 'Confections', 'Desserts, candies, and sweet breads');
+
+INSERT INTO Shippers_Table(ShipperID, ShipperName, Phone)
+VALUES
+(1, 'Speedy Express', 5035559831),
+(2, 'United Package', 5035553199),
+(3, 'Federal Shipping', 5035559931);
 
 
 -- ALTER TABLE table_name
